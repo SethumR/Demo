00522c4a961fbf73846841f3eaf239d7303f778d
Sethum <sethumgelaka6@gmail.com>
2024-06-11 10:47:07 +0530

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ function App() {
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
            Aliquam odio magnam consequatur dolores temporibus vel veniam quasi 
            nesciunt doloremque quidem! Voluptatem et at reiciendis. Esse rerum quos dignissimos voluptate quas.</p>
+
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis amet, nobis totam aliquam voluptatem, velit atque, tempora deleniti ea ducimus ipsa nesciunt ut fuga ullam numquam excepturi. Corporis, esse consequatur.
     </Body>
 
     </>
